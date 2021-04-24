47210f85a775f90bbc8940efeb9435144a07d3d9
Dylan Fries <dylan.fries@connect.7oaks.org>
2021-04-24 11:10:53 -0500

    Rock Trap
    Added Rock fall trap

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    [Header("Falling/Damage")]
+    public bool isKnockedDown = false;
+    public UnityEvent knockedDownEvent = new UnityEvent();
+    public UnityEvent getUpEvent = new UnityEvent();
+    public float getUpTimer = 1f;
+
+    [Header("Movement")]
     public float xForce = 1f;
     public float yForce = 1f;
     public float jumpForce = 1f;
 
-    public float verticalMoveRatio = 2f / 3f; // how far to move up to correspond with moving horizontal. 
+    [Header("Movement Settings")]
+    private float verticalMoveRatio = 2f / 3f; // how far to move up to correspond with moving horizontal. 
 
+    [Header("Debugging Input")]
     public Vector2 joyInput = new Vector2();
+
 
     public Rigidbody2D rigid;
 
@@ -27,13 +38,32 @@ public class Player : MonoBehaviour
     }
 
     public void UpdateInput(){
+        if (isKnockedDown)
+            return;
+
         joyInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         rigid.AddForce(new Vector2(joyInput.x * xForce, joyInput.y * yForce * verticalMoveRatio ));
 
         if( Input.GetButtonDown("Jump")){
-            rigid.AddForce(new Vector2(joyInput.x * xForce, joyInput.y * yForce * verticalMoveRatio), ForceMode2D.Impulse);
+            Vector2 normalizedInput = joyInput.normalized;
+
+            Vector2 baseVector = new Vector2(normalizedInput.x * xForce, normalizedInput.y * yForce * verticalMoveRatio);
+            Vector2 directionVector = new Vector2 (joyInput.x * xForce, joyInput.y * yForce * verticalMoveRatio);
+            rigid.AddForce((baseVector + directionVector), ForceMode2D.Impulse);
         }
     }
 
-    
+    public void KnockedDown(){
+        if (isKnockedDown)
+            return;
+
+        knockedDownEvent.Invoke();
+        StartCoroutine("GetUp");
+    }
+
+    private IEnumerable GetUp(){
+        yield return new WaitForSeconds(getUpTimer);
+        isKnockedDown = false;
+        getUpEvent.Invoke();
+    }
 }
--- Assets/CollisionTrigger.cs
+++ Assets/CollisionTrigger.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CollisionTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool fireOnce = true;
+    public bool hasBeenTriggered = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public UnityEvent triggeredEvent = new UnityEvent();
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(fireOnce && !hasBeenTriggered){
+            triggeredEvent.Invoke();
+        }
     }
 }
