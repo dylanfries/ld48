626d6270b2fc1897384ee5b8ffe3cf68054397ba
Dylan Fries <dylan.fries@connect.7oaks.org>
2021-04-24 15:41:40 -0500

    Basic Text pop up window
    Basic Text pop up window

---
+++ Assets/TextUIPopup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUIPopup : MonoBehaviour
+{
+    public GameObject UIPanel;
+    public Text textBox;
+
+    public int timesOpened = 0;
+
+    public string[] textToPrint;
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        UIPanel.SetActive(true);
+
+        if (timesOpened < textToPrint.Length)
+            textBox.text = textToPrint[timesOpened];
+        else{
+            textBox.text = textToPrint[textToPrint.Length - 1];
+        }
+
+        timesOpened++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        UIPanel.SetActive(false);
+    }
+}
