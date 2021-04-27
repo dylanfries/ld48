b16cbdaa82b87289fe2fa8a4f1c0c4a0c49c2ab1
Dylan Fries <dylan.fries@connect.7oaks.org>
2021-04-27 15:44:43 -0500

    Some post launch performance bug fixes
    Reimport art with point mode on, lock player position at start, disable transparent overlay for performance.

--- Assets/OnStart.cs
+++ Assets/OnStart.cs
@@ -6,11 +6,19 @@ using UnityEngine.Events;
 public class OnStart : MonoBehaviour
 {
     public UnityEvent onStartEvent = new UnityEvent();
+    public UnityEvent onDelayedStart = new UnityEvent();
+    public float delayTimer = 6;
 
     // Start is called before the first frame update
     void Start()
     {
         onStartEvent.Invoke();
+        StartCoroutine(DelayedEvent(delayTimer));
+    }
+
+    private IEnumerator DelayedEvent(float t){
+        yield return new WaitForSeconds(t);
+        onDelayedStart.Invoke();
     }
    
 }
